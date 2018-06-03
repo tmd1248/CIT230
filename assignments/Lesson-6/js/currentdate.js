3459b2d2bc4c8cec1f15b8ae87e3593587bfa4f9
tyler day <tmd1248@gmail.com>
2018-06-02 18:47:27 -0600

    Hopefully Final lesson 6 update

--- assignments/Lesson-6/js/currentdate.js
+++ assignments/Lesson-6/js/currentdate.js
@@ -1,10 +1,10 @@
 var date = new Date();
 var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var weekDate = weekday[date.getDay];
+var weekDate = weekday[date.getDay()];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "november", "December"];
-var day = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-var currentMonth = month[date.getMonth];
-var currentDate = day[date.getDate];
+var day = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+var currentMonth = month[date.getMonth()];
+var currentDate = day[date.getDate()];
 
 
 document.getElementById("currentDate").innerHTML = weekDate + ", " + currentDate + " " + currentMonth + " "  + date.getFullYear();
